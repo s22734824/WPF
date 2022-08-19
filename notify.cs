3811fb5157f38ac86ba57ecc95458236e46791a6
s22734824 <40797645+s22734824@users.noreply.github.com>
2022-08-19 11:37:57 +0800

    Notify
    WPF 右小角圖案

---
+++ notify.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    class notify
+    {
+        System.Windows.Forms.NotifyIcon n;
+        System.Windows.Forms.ContextMenuStrip nIconMenu;
+        public notify(Action Closing,Action DoubleClick)
+        {
+            n = new System.Windows.Forms.NotifyIcon();
+            n.DoubleClick += (object sender, EventArgs e) => { DoubleClick(); };
+             n.Icon = new System.Drawing.Icon("HYTE_LOGO.ico");
+            nIconMenu = new System.Windows.Forms.ContextMenuStrip();
+            System.Windows.Forms.ToolStripMenuItem nIconMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            nIconMenuItem.Text = "exit()";
+            nIconMenuItem.Click += (object sender, EventArgs e) => { Closing(); };
+            System.Windows.Forms.ToolStripMenuItem nIconMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            nIconMenuItem2.Text = "exit2()";
+            nIconMenuItem2.Click += (object sender, EventArgs e) => { Closing(); };
+            nIconMenuItem.DropDownItems.Add(nIconMenuItem2);
+            nIconMenu.Items.Add(nIconMenuItem);
+            n.ContextMenuStrip = nIconMenu;
+            n.Visible = true;
+        }
+
+
+        public void ShowIcon()
+        {
+            n.Visible = true;
+        }
+
+        public void Closing()
+        {
+            n.Visible = false;
+        }
+    }
+}
