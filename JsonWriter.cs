57c5126ae8776e5e6709f3c8485e3099a534337a
s22734824 <40797645+s22734824@users.noreply.github.com>
2023-04-19 08:17:24 +0800

    JaonVersion
    JaonVersion

---
+++ JsonWriter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json; 
+namespace huef
+{
+    class SaveModel
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+    class JsonWriter
+    {
+        static Action FlushList;
+        static bool First;
+        List<SaveModel> T;
+        SaveModel Model;
+        
+        public void SetValue(string value)
+        {
+            
+        }
+        public string GetValue()
+        {
+            return Model.Value;
+        }
+
+        public JsonWriter(string Name, string InitValue)
+        {
+            if (!First)
+            {
+                if (File.Exists("config.json"))
+                {
+                    
+                }
+            }
+
+
+
+            //Model = new SaveModel() {Name = Name,Value = Value };
+        }
+        private void WriteAllObjectToFile()
+        {
+            
+        }
+        private void AddObjectToList()
+        {
+
+        }
+
+
+
+    }
+}
